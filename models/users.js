84ae3bf6c7c5dc8d520f54700788083ffc622d34
Jules Ballaran <jules.ballaran@boom.camp>
2020-03-13 02:50:03 +0000

    Users

--- models/users.js
+++ models/users.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 const { dynamoose } = require('../config')
 const { v4: uuidv4 } = require('uuid')
 const users = dynamoose.model('billing-users', {
-=======
-const { dynamoose } = require("../config");
-const { v4: uuidv4 } = require("uuid");
-const users = dynamoose.model("billing-user", {
->>>>>>> 9c08711a688e329915e11d391dc2732effb33286
   id: String,
   googleId: String,
   email: String,
@@ -17,20 +11,20 @@ const users = dynamoose.model("billing-user", {
   type: String,
   company: String,
   status: String
-});
+})
 
 module.exports = {
   add: async (req, res) => {
     const add = new users({
       id: uuidv4(),
       ...req.body
-    });
+    })
     add
       .save()
       .then(result => res.status(201).json(result))
       .catch(err => {
-        res.status(500).json(err);
-      });
+        res.status(500).json(err)
+      })
   },
   list: (req, res) => {
     users
@@ -66,4 +60,4 @@ module.exports = {
       .then(result => res.status(201).json(result))
       .catch(err => res.status(500).json(err))
   }
-};
+}
