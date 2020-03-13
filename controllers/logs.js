f05ad6e8725b7dd8e0f8f2f6e63a669c47da8dd1
Jules Ballaran <jules.ballaran@boom.camp>
2020-03-13 05:42:17 +0000

    Users

--- controllers/logs.js
+++ controllers/logs.js
@@ -1,14 +1,6 @@
-const { dynamoose } = require("../config");
-const { v4: uuidv4 } = require("uuid");
-const dateUtil = require("../utils/date");
-const logs = dynamoose.model("billing-logs", {
-  id: String,
-  date: String,
-  time: String,
-  type: String,
-  description: String,
-  invoiceId: String
-});
+const { logs } = require('../models')
+const { v4: uuidv4 } = require('uuid')
+const dateUtil = require('../utils/date')
 
 module.exports = {
   add: async (req, res) => {
@@ -17,13 +9,13 @@ module.exports = {
       date: dateUtil.getDate(),
       time: dateUtil.getTime(),
       ...req.body
-    });
+    })
     add
       .save()
       .then(result => res.status(201).json(result))
       .catch(err => {
-        res.status(500).json(err);
-      });
+        res.status(500).json(err)
+      })
   },
   list: (req, res) => {
     logs
@@ -32,7 +24,7 @@ module.exports = {
       .exec()
       .then(result => res.status(200).json(result))
       .catch(err => {
-        res.status(500).json(err);
-      });
+        res.status(500).json(err)
+      })
   }
-};
+}
