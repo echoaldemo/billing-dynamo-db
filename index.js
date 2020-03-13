377d3fd586d1f52753e2fc800bdee45f624b0e01
Jules Ballaran <jules.ballaran@boom.camp>
2020-03-13 02:45:36 +0000

    add user

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const serverless = require("serverless-http");
-const express = require("express");
-const bodyParser = require("body-parser");
-const users = require("./models/users");
-const logs = require("./models/logs");
-const app = express();
-app.use(bodyParser.json({ strict: false }));
+const serverless = require('serverless-http')
+const express = require('express')
+const bodyParser = require('body-parser')
+const users = require('./models/users')
+const logs = require('./models/logs')
+const app = express()
+app.use(bodyParser.json({ strict: false }))
 
-app.post("/api/users/create", users.add);
-app.get("/api/users/list", users.list);
+app.post('/api/users/create', users.add)
+app.get('/api/users/list', users.list)
+app.get('/api/users/:id', users.getById)
+app.delete('/api/users/delete/:id', users.delete)
+app.patch('/api/users/edit/:id', users.edit)
 
-app.post("/api/logs/create", logs.add);
-app.get("/api/logs/list", logs.list);
+app.post('/api/logs/create', logs.add)
+app.get('/api/logs/list', logs.list)
 
-module.exports.handler = serverless(app);
+module.exports.handler = serverless(app)
